18a1a89f56f4bcb2d8059098ab0bbf7ab722e91f
Michael Herold <michael.j.herold@gmail.com>
2015-06-11 15:18:40 -0500

    Add support and docs for merchant_paypal_account

--- lib/accepton/api/tokenization.rb
+++ lib/accepton/api/tokenization.rb
@@ -18,11 +18,19 @@ module AcceptOn
       # @example Create a transaction token with a description and application fee
       #   create_token(amount: 10_99, description: 'Test', application_fee: 99)
       #
+      # @example Create a Paypal-specific token that pays a merchant and charges
+      #          them an application fee
+      #   create_token(amount: 15_00, description: 'Test', application_fee: 1_50,
+      #                merchant_paypal_account: 'merchant@example.com')
+      #
       # @param args [Hash] Attributes to set to the transaction token
       # @option args [Integer] :amount The amount in cents of the transaction
-      # @option args [String]  :description A description of the transaction
       # @option args [Integer] :application_fee The application fee to be passed on to the processor
       # @option args [String]  :currency The currency to charge in (default: usd)
+      # @option args [String]  :description A description of the transaction
+      # @option args [String]  :merchant_paypal_account The merchant's Paypal
+      #                        account when you want to pay a merchant instead of
+      #                        yourself. Can be used with an application fee.
       #
       # @raise [AcceptOn::Error] If an API error happens
       # @return [AcceptOn::TransactionToken]
--- lib/accepton/transaction_token.rb
+++ lib/accepton/transaction_token.rb
@@ -1,11 +1,16 @@
 module AcceptOn
   class TransactionToken
-    # @attr_reader amount [Integer] The amount of the transaction
+    # @attr_reader amount [Integer] The amount of the transaction in cents
+    # @attr_reader application_fee [Integer] The amount of application fee
     # @attr_reader created [DateTime] The time the transaction was created
     # @attr_reader description [String] A description of the transaction
     # @attr_reader id [String] The transaction identifier
+    # @attr_reader merchant_paypal_account [String] The merchant's Paypal
+    #              account when you want to pay a merchant instead of
+    #              yourself. Can be used with an application fee.
     # @api public
-    attr_reader :amount, :created, :description, :id
+    attr_reader :amount, :application_fee, :created, :description, :id,
+                :merchant_paypal_account
 
     # Creates a model representation of a transaction token
     #
